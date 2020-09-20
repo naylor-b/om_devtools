ef39f2d92c47415b7eb4a29efe269e56e0cd9578
Bret Naylor <naylor.b@gmail.com>
2020-09-20 08:13:05 -0400

    interim

--- om_devtools/omdbg.py
+++ om_devtools/omdbg.py
@@ -4,7 +4,7 @@ import sys
 import pdb
 import bdb
 import inspect
-from bdb import effective, Breakpoint, checkfuncname
+from bdb import effective, Breakpoint, checkfuncname, BdbQuit
 
 from om_devtools.breakpoints import BreakpointLocator, BreakManager
 import openmdao.utils.hooks as hooks
@@ -12,14 +12,18 @@ from openmdao.utils.file_utils import _load_and_exec, _to_filename
 
 
 class _DBGInput(object):
-    def __init__(self, brkmanager):
-        self.brkmanager = brkmanager
+    def __init__(self, debugger):
+        self.brkmanager = debugger.brkmanager
+        self.debugger = debugger
 
     def readline(self, *args, **kwargs):
         if self.brkmanager.has_commands():
             return self.brkmanager.next_command()
         else:
-            return input('Enter a command: ')
+            inp = input('Enter a command: ')
+            if not inp:
+                return 'n'
+            return inp
 
 
 class OMdbg(pdb.Pdb):
@@ -28,7 +32,7 @@ class OMdbg(pdb.Pdb):
 
     def __init__(self, problem, *args, **kwargs):
         self.brkmanager = BreakManager(problem)
-        kwargs['stdin'] = _DBGInput(self.brkmanager)
+        kwargs['stdin'] = _DBGInput(self)
         super(OMdbg, self).__init__(*args, **kwargs)
         self.prompt = ''
         self.command_prompt = ''
@@ -37,6 +41,7 @@ class OMdbg(pdb.Pdb):
     def do_stopin(self, arg):
         'Stop in a specific instance method'
         self.brkmanager.stop_in(arg)
+        return True
 
     # def do_bye(self, arg):
     #     'Close the openmdao window and exit:  BYE'
@@ -48,161 +53,161 @@ class OMdbg(pdb.Pdb):
     def message(self, msg):
         print(msg, file=self.stdout)
 
-    def break_here(self, frame):
-        """Return True if there is an effective breakpoint for this line.
+    # def break_here(self, frame):
+    #     """Return True if there is an effective breakpoint for this line.
 
-        Check for line or function breakpoint and if in effect.
-        Delete temporary breakpoints if effective() says to.
-        """
-        filename = self.canonic(frame.f_code.co_filename)
-        if filename not in self.breaks:
-            return False
-        lineno = frame.f_lineno
-        if lineno not in self.breaks[filename]:
-            # The line itself has no breakpoint, but maybe the line is the
-            # first line of a function with breakpoint set by function name.
-            lineno = frame.f_code.co_firstlineno
-            if lineno not in self.breaks[filename]:
-                return False
+    #     Check for line or function breakpoint and if in effect.
+    #     Delete temporary breakpoints if effective() says to.
+    #     """
+    #     filename = self.canonic(frame.f_code.co_filename)
+    #     if filename not in self.breaks:
+    #         return False
+    #     lineno = frame.f_lineno
+    #     if lineno not in self.breaks[filename]:
+    #         # The line itself has no breakpoint, but maybe the line is the
+    #         # first line of a function with breakpoint set by function name.
+    #         lineno = frame.f_code.co_firstlineno
+    #         if lineno not in self.breaks[filename]:
+    #             return False
 
-        # flag says ok to delete temp. bp
-        (bp, flag) = self.my_effective(filename, lineno, frame)
-        if bp:
-            self.currentbp = bp.number
-            if (flag and bp.temporary):
-                self.do_clear(str(bp.number))
-            return True
-        else:
-            return False
+    #     # flag says ok to delete temp. bp
+    #     (bp, flag) = self.my_effective(filename, lineno, frame)
+    #     if bp:
+    #         self.currentbp = bp.number
+    #         if (flag and bp.temporary):
+    #             self.do_clear(str(bp.number))
+    #         return True
+    #     else:
+    #         return False
 
-    # Determines if there is an effective (active) breakpoint at this
-    # line of code.  Returns breakpoint number or 0 if none
-    def my_effective(self, file, line, frame):
-        """Determine which breakpoint for this file:line is to be acted upon.
+    # # Determines if there is an effective (active) breakpoint at this
+    # # line of code.  Returns breakpoint number or 0 if none
+    # def my_effective(self, file, line, frame):
+    #     """Determine which breakpoint for this file:line is to be acted upon.
 
-        Called only if we know there is a breakpoint at this location.  Return
-        the breakpoint that was triggered and a boolean that indicates if it is
-        ok to delete a temporary breakpoint.  Return (None, None) if there is no
-        matching breakpoint.
-        """
-        for b in Breakpoint.bplist[file, line]:
-            if not b.enabled:
-                continue
-            if not checkfuncname(b, frame):
-                continue
-            # Count every hit when bp is enabled
-            b.hits += 1
-            if not b.cond:
-                # If unconditional, and ignoring go on to next, else break
-                if b.ignore > 0:
-                    b.ignore -= 1
-                    continue
-                else:
-                    # breakpoint and marker that it's ok to delete if temporary
-                    return (b, True)
-            else:
-                # Conditional bp.
-                # Ignore count applies only to those bpt hits where the
-                # condition evaluates to true.
-                try:
-                    if b.cond.startswith('@'):
-                        self.prompt=''
-                        val = self.brkmanager.do_break_action(b, inspect.getframeinfo(frame),
-                                                              frame.f_globals, frame.f_locals)
-                    else:
-                        self.prompt='<omdbg> '
-                        val = eval(b.cond, frame.f_globals, frame.f_locals)
-                    if val:
-                        if b.ignore > 0:
-                            b.ignore -= 1
-                            # continue
-                        else:
-                            return (b, True)
-                    # else:
-                    #   continue
-                except:
-                    # if eval fails, most conservative thing is to stop on
-                    # breakpoint regardless of ignore count.  Don't delete
-                    # temporary, as another hint to user.
-                    return (b, False)
-        return (None, None)
+    #     Called only if we know there is a breakpoint at this location.  Return
+    #     the breakpoint that was triggered and a boolean that indicates if it is
+    #     ok to delete a temporary breakpoint.  Return (None, None) if there is no
+    #     matching breakpoint.
+    #     """
+    #     for b in Breakpoint.bplist[file, line]:
+    #         if not b.enabled:
+    #             continue
+    #         if not checkfuncname(b, frame):
+    #             continue
+    #         # Count every hit when bp is enabled
+    #         b.hits += 1
+    #         if not b.cond:
+    #             # If unconditional, and ignoring go on to next, else break
+    #             if b.ignore > 0:
+    #                 b.ignore -= 1
+    #                 continue
+    #             else:
+    #                 # breakpoint and marker that it's ok to delete if temporary
+    #                 return (b, True)
+    #         else:
+    #             # Conditional bp.
+    #             # Ignore count applies only to those bpt hits where the
+    #             # condition evaluates to true.
+    #             try:
+    #                 if b.cond.startswith('@'):
+    #                     self.prompt=''
+    #                     val = self.brkmanager.do_break_action(b, inspect.getframeinfo(frame),
+    #                                                           frame.f_globals, frame.f_locals)
+    #                 else:
+    #                     self.prompt='<omdbg> '
+    #                     val = eval(b.cond, frame.f_globals, frame.f_locals)
+    #                 if val:
+    #                     if b.ignore > 0:
+    #                         b.ignore -= 1
+    #                         # continue
+    #                     else:
+    #                         return (b, True)
+    #                 # else:
+    #                 #   continue
+    #             except:
+    #                 # if eval fails, most conservative thing is to stop on
+    #                 # breakpoint regardless of ignore count.  Don't delete
+    #                 # temporary, as another hint to user.
+    #                 return (b, False)
+    #     return (None, None)
 
-    def do_commands(self, arg):
-        """commands [bpnumber]
-        (com) ...
-        (com) end
-        (Pdb)
+    # def do_commands(self, arg):
+    #     """commands [bpnumber]
+    #     (com) ...
+    #     (com) end
+    #     (Pdb)
 
-        Specify a list of commands for breakpoint number bpnumber.
-        The commands themselves are entered on the following lines.
-        Type a line containing just 'end' to terminate the commands.
-        The commands are executed when the breakpoint is hit.
+    #     Specify a list of commands for breakpoint number bpnumber.
+    #     The commands themselves are entered on the following lines.
+    #     Type a line containing just 'end' to terminate the commands.
+    #     The commands are executed when the breakpoint is hit.
 
-        To remove all commands from a breakpoint, type commands and
-        follow it immediately with end; that is, give no commands.
+    #     To remove all commands from a breakpoint, type commands and
+    #     follow it immediately with end; that is, give no commands.
 
-        With no bpnumber argument, commands refers to the last
-        breakpoint set.
+    #     With no bpnumber argument, commands refers to the last
+    #     breakpoint set.
 
-        You can use breakpoint commands to start your program up
-        again.  Simply use the continue command, or step, or any other
-        command that resumes execution.
+    #     You can use breakpoint commands to start your program up
+    #     again.  Simply use the continue command, or step, or any other
+    #     command that resumes execution.
 
-        Specifying any command resuming execution (currently continue,
-        step, next, return, jump, quit and their abbreviations)
-        terminates the command list (as if that command was
-        immediately followed by end).  This is because any time you
-        resume execution (even with a simple next or step), you may
-        encounter another breakpoint -- which could have its own
-        command list, leading to ambiguities about which list to
-        execute.
+    #     Specifying any command resuming execution (currently continue,
+    #     step, next, return, jump, quit and their abbreviations)
+    #     terminates the command list (as if that command was
+    #     immediately followed by end).  This is because any time you
+    #     resume execution (even with a simple next or step), you may
+    #     encounter another breakpoint -- which could have its own
+    #     command list, leading to ambiguities about which list to
+    #     execute.
 
-        If you use the 'silent' command in the command list, the usual
-        message about stopping at a breakpoint is not printed.  This
-        may be desirable for breakpoints that are to print a specific
-        message and then continue.  If none of the other commands
-        print anything, you will see no sign that the breakpoint was
-        reached.
-        """
-        if not arg:
-            bnum = len(bdb.Breakpoint.bpbynumber) - 1
-        else:
-            try:
-                bnum = int(arg)
-            except:
-                self.error("Usage: commands [bnum]\n        ...\n        end")
-                return
-        self.commands_bnum = bnum
-        # Save old definitions for the case of a keyboard interrupt.
-        if bnum in self.commands:
-            old_command_defs = (self.commands[bnum],
-                                self.commands_doprompt[bnum],
-                                self.commands_silent[bnum])
-        else:
-            old_command_defs = None
-        self.commands[bnum] = []
-        self.commands_doprompt[bnum] = True
-        self.commands_silent[bnum] = False
+    #     If you use the 'silent' command in the command list, the usual
+    #     message about stopping at a breakpoint is not printed.  This
+    #     may be desirable for breakpoints that are to print a specific
+    #     message and then continue.  If none of the other commands
+    #     print anything, you will see no sign that the breakpoint was
+    #     reached.
+    #     """
+    #     if not arg:
+    #         bnum = len(bdb.Breakpoint.bpbynumber) - 1
+    #     else:
+    #         try:
+    #             bnum = int(arg)
+    #         except:
+    #             self.error("Usage: commands [bnum]\n        ...\n        end")
+    #             return
+    #     self.commands_bnum = bnum
+    #     # Save old definitions for the case of a keyboard interrupt.
+    #     if bnum in self.commands:
+    #         old_command_defs = (self.commands[bnum],
+    #                             self.commands_doprompt[bnum],
+    #                             self.commands_silent[bnum])
+    #     else:
+    #         old_command_defs = None
+    #     self.commands[bnum] = []
+    #     self.commands_doprompt[bnum] = True
+    #     self.commands_silent[bnum] = False
 
-        prompt_back = self.prompt
-        self.prompt = self.command_prompt
-        self.commands_defining = True
-        try:
-            self.cmdloop()
-        except KeyboardInterrupt:
-            # Restore old definitions.
-            if old_command_defs:
-                self.commands[bnum] = old_command_defs[0]
-                self.commands_doprompt[bnum] = old_command_defs[1]
-                self.commands_silent[bnum] = old_command_defs[2]
-            else:
-                del self.commands[bnum]
-                del self.commands_doprompt[bnum]
-                del self.commands_silent[bnum]
-            self.error('command definition aborted, old commands restored')
-        finally:
-            self.commands_defining = False
-            self.prompt = prompt_back
+    #     prompt_back = self.prompt
+    #     self.prompt = self.command_prompt
+    #     self.commands_defining = True
+    #     try:
+    #         self.cmdloop()
+    #     except KeyboardInterrupt:
+    #         # Restore old definitions.
+    #         if old_command_defs:
+    #             self.commands[bnum] = old_command_defs[0]
+    #             self.commands_doprompt[bnum] = old_command_defs[1]
+    #             self.commands_silent[bnum] = old_command_defs[2]
+    #         else:
+    #             del self.commands[bnum]
+    #             del self.commands_doprompt[bnum]
+    #             del self.commands_silent[bnum]
+    #         self.error('command definition aborted, old commands restored')
+    #     finally:
+    #         self.commands_defining = False
+    #         self.prompt = prompt_back
 
 
 # ---------------- command line tool setup -------------------
